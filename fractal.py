78095191370a9b124e1311dd4224fc635e74d944
Nate Gonzales Hess <nhess@uoregon.edu>
2022-12-08 06:41:16 -0800

    ipl fractal class

--- fractal.py
+++ fractal.py
@@ -61,7 +61,10 @@ class Generate:
                 cv2.imshow('Fractal Preview', self.pattern[k, :, :])
                 cv2.waitKey(16)
 
-    def preview2d(self, size=256):
+    def preview2d(self, index=-1, size=256):
+        assert 0 < index <= 100, "Index must be between 1-100"
+        if index != -1:
+            frame = int((index/100)*self.size)
         # 2d grayscale and BW previews
         if self.dimension == 2:
             preview = cv2.resize(self.pattern, [size, size], interpolation=cv2.INTER_AREA)
@@ -73,7 +76,7 @@ class Generate:
             plt.show()
         # 2d slices of 3d fractals for preview
         if self.dimension == 3:
-            preview = cv2.resize(self.pattern[-1, :, :], [size, size], interpolation=cv2.INTER_AREA)
+            preview = cv2.resize(self.pattern[frame, :, :], [size, size], interpolation=cv2.INTER_AREA)
             prev_bw = (preview > .5)
             previews = [preview, prev_bw]
             for i in range(2):
